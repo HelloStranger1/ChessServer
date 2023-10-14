79b791549432869b14e96a21c6ed39077008e9d4
Eyal Ben Natan <eyalbenatan@gmail.com>
2023-10-14 11:42:15 +0300

    Deleted an unused class, updated Spring version to 3.1.4 and changed Postgresql database name.

--- src/main/java/com/hellostranger/chessserver/storage/FENRepresentationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hellostranger.chessserver.storage;
-
-import com.hellostranger.chessserver.models.entities.FENRepresentation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FENRepresentationRepository extends JpaRepository<FENRepresentation, Integer> {
-}
